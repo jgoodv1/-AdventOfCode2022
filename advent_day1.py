6bfaaec33c85dbd48b748d2762a5bddbae85f540
Jonathan Good <119594359+jgoodv1@users.noreply.github.com>
2022-12-03 17:37:50 +0000

    Add files via upload

---
+++ advent_day1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 11:43:05 2022
+
+@author: jonathangood
+"""
+
+total_calories = []
+count = 0
+
+import csv
+with open('input1.txt', 'r') as fd:
+    reader = csv.reader(fd)
+    for row in reader:
+        if row:
+            count += int(row[0])
+        else:
+            total_calories.append(count)
+            count = 0
+            
+print(max(total_calories))
+
+total_calories.sort(reverse=True)
+print(sum(total_calories[0:3]))
