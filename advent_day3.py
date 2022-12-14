821b86dfad13f4e624aa6eb0fe0178017926462f
Jonathan Good <jonathan.good@version1.com>
2022-12-14 11:20:42 +0000

    Update Advent of Code Day 3
    Added timings and changed file input

--- advent_day3.py
+++ advent_day3.py
@@ -5,6 +5,13 @@ Created on Sat Dec  3 10:00:29 2022
 
 @author: jonathangood
 """
+import time
+start = time.time()
+file, sets = [], []
+part1, part2, counter= 0, 0, 0
+
+with open("input3.txt") as fd:
+    file = fd.read().strip().split("\n")
 
 def getpriorities(item):
     result = 0
@@ -16,19 +23,7 @@ def getpriorities(item):
 
     return result
 
-file, sets = [], []
-result_a, result_b, counter= 0, 0, 0
-
-file = []
-import csv
-with open('input3.txt', 'r') as fd:
-    reader = csv.reader(fd)
-    for row in reader:
-        items = row[0]
-        file.append(items)
-
 for items in file:
-
     """
     Part 1
     """
@@ -45,7 +40,7 @@ for items in file:
 
     dupes = set(rucksack1).intersection(rucksack2)
     for dupe in dupes:
-        result_a += getpriorities(dupe)
+        part1 += getpriorities(dupe)
 
     """
     Part 2
@@ -55,11 +50,11 @@ for items in file:
     if counter%3 == 0:
         dupes = set(sets[0]).intersection(sets[1], sets[2])
         for dupe in dupes:
-            result_b += getpriorities(dupe)
+            part2 += getpriorities(dupe)
         sets = []
 
 # Part 1 Answer
-print(result_a)
+print('Part 1: ', part1)
 
 # Part 2 Answer
-print(result_b)
+print('Part 2: ', part2,'\nTotal time:', (time.time()-start),' seconds')
